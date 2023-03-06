67dc1c7e5e21478becab83038b1b5cb5866f1529
peibinzhu <760425373@qq.com>
2023-03-06 10:44:28 +0800

    实现 ProviderConfigInterface

--- src/UtilServiceProvider.php
+++ src/UtilServiceProvider.php
@@ -6,10 +6,11 @@ namespace PeibinLaravel\Utils;
 
 use Illuminate\Support\ServiceProvider;
 use PeibinLaravel\Contracts\StdoutLoggerInterface;
+use PeibinLaravel\ProviderConfig\Contracts\ProviderConfigInterface;
 use PeibinLaravel\Utils\Contracts\Formatter;
 use PeibinLaravel\Utils\ExceptionHandler\DefaultFormatter;
 
-class UtilServiceProvider extends ServiceProvider
+class UtilServiceProvider extends ServiceProvider implements ProviderConfigInterface
 {
     public function __invoke(): array
     {
